d39b32cb2768548ee436a5bd338caa73c729ef39
raphael <simon.raphael@gmail.com>
2015-06-27 14:36:34 -0700

    Update AUR package build system

---
+++ aur/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+REPO=$DIR/linux-samus4
+if [ ! -d $REPO ]; then
+  git clone ssh://aur@aur4.archlinux.org/linux-samus4 $REPO
+  if [ $? -ne 0 ]; then
+    echo Failed to clone linux-samus4
+    exit 1
+  fi
+fi
+cd $DIR
+
+files[0]="config"
+files[1]="linux.install"
+files[2]="linux.preset"
+files[3]="PKGBUILD"
+
+for f in "${files[@]}"; do
+  cp -L $DIR/$f $REPO
+done
+cd $REPO
+mksrcinfo
+if [ $? -ne 0 ]; then
+  echo Failed to run mksrcinfo
+  exit 1
+fi
+echo $REPO updated.
