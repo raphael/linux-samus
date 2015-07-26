2074ba724b1a332a7c6bff3b0a1072002e65f37a
raphael <simon.raphael@gmail.com>
2015-07-26 15:50:34 -0700

    Make xaccel.sh script work on Ubuntu 15.04

--- scripts/setup/xaccel.sh
+++ scripts/setup/xaccel.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo cp xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d
+if [ -d /etc/X11/xorg.conf.d ]; then
+  sudo cp xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d
+elif [ -d /usr/share/X11/xorg.conf.d ]; then
+  sudo cp xorg.conf.d/20-intel.conf /usr/share/X11/xorg.conf.d
+else
+  echo "failed to find xorg.conf.d directory"
+  exit 1
+fi
