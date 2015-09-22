68a4e1c8d3715f90a6d9880094117b5eef6328aa
Raphael Simon <simon.raphael@gmail.com>
2015-09-21 18:03:59 -0700

    Start work on PPA

--- scripts/debian/build.sh
+++ scripts/debian/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export FLAVOUR=samus
+export DEBEMAIL="simon.raphael@gmail.com"
+export DEBFULLNAME="Raphael Simon"
+export KDEB_CHANGELOG_DIST=vivid
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LINUX=`readlink -f $DIR/../../build/linux-patched`
-DEBIAN=`readlink -f $LINUX/../debian`
+DEBIANPATH=`readlink -f $LINUX/../debian`
 
 if [ ! -d $LINUX ]; then
   echo "The patched tree must be generated first, couldn't find it at $LINUX"
@@ -10,14 +15,31 @@ if [ ! -d $LINUX ]; then
   exit 1
 fi
 
+rm -rf $LINUX/debian
+rm -rf $LINUX/debian.master
+cp -r debian $LINUX
+cp -r debian.master $LINUX
 cd $LINUX
-echo `pwd`
-export KDEB_CHANGELOG_DIST=vivid
-make deb-pkg
+
+# Now build source package
+#export KERNELVERSION=$(make EXTRAVERSION='' kernelversion) 
+#rm -f debian/changelog
+#dch --create --changelog debian/changelog --package linux-$FLAVOUR -v ${KERNELVERSION}
+dch -r ""
+VERSION="`sed -n '1 s/^.*(\(.*\)).*/\1/p' debian/changelog`"
+PPA_FILE="`make --no-print-directory -f debian/rules print-ppa-file-name`"
+KEYID="`gpg --list-keys ${DEBEMAIL} | sed -n 's,^pub.*/\([^ ]*\).*,\1,p'`"
+# with -nc size is 315232140
+dpkg-buildpackage -j4 -S -sa -k${KEYID} -rfakeroot -I.git -I.gitignore -i'\.git.*'
 if [ $? -ne 0 ]; then
   echo "** Build failed, aborting"
   exit 1
 fi
-mkdir -p $DEBIAN
-mv ../*.deb $DEBIAN
-echo "Debian packages generated in $DEBIAN"
+mkdir -p $DEBIANPATH
+rm -rf $DEBIANPATH/*
+rm ../*.deb
+mv ../*.changes $DEBIANPATH
+mv ../*.dsc $DEBIANPATH
+mv ../*.tar.gz $DEBIANPATH
+cd $DEBIANPATH
+dput ppa:simon-raphael/linux-samus ./*.changes
