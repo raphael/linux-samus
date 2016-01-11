513895347f69d72596109bfa2ba9c8f6df5b2fea
Raphael Simon <simon.raphael@gmail.com>
2016-01-11 09:29:41 -0800

    Integrate @ehegnes improvements

--- scripts/setup/touchpad/enable-atmel.sh
+++ scripts/setup/touchpad/enable-atmel.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-sudo modprobe i2c-dev
-echo -ne 'r\nq\n' | sudo ./mxt-app -d i2c-dev:7-004a
+# This script resets the Atmel maXTouch chips that control the touchscreen and
+# touchpad input devices.
+
+# It is recommended for this script to be run by the init system on boot and
+# after resuming from suspend.
+
+# Note the use of "mxt-app" which would have to be available to the init system.
+
+# Elevate privileges if necessary
+PROMPT=sudo
+[ "$UID" -eq 0 ] || exec $PROMPT $SHELL "$0" "$@"
+
+# Load `i2c-dev` module to access I2C devices through /dev
+$PROMPT modprobe i2c-dev &>/dev/null
+
+# Reset controllers
+echo -ne 'r\nq\n' | $PROMPT ./mxt-app -d i2c-dev:{7,8}-004a &>/dev/null
