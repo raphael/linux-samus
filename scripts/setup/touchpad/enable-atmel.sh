86d6881feb600fdd7de6bf63479acd81bd035362
Raphael Simon <simon.raphael@gmail.com>
2016-01-16 23:11:40 -0800

    Make touchpad enable script try all i2c devices

--- scripts/setup/touchpad/enable-atmel.sh
+++ scripts/setup/touchpad/enable-atmel.sh
@@ -16,4 +16,22 @@ PROMPT=sudo
 $PROMPT modprobe i2c-dev &>/dev/null
 
 # Reset controllers
-echo -ne 'r\nq\n' | $PROMPT ./mxt-app -d i2c-dev:{7,8}-004a &>/dev/null
+# Touchpad - seems to be using i2c dev 0 or 7
+FOUND=0
+DEV=0
+while [ $FOUND -ne 1 ]; do
+  OUT=$(echo -ne 'i\nq\n' | $PROMPT ./mxt-app -d i2c-dev:$DEV-004a 2>/dev/null)
+  if [[ $OUT == *"Atmel maXTouch"* ]]; then
+    FOUND=1
+  else
+    ((DEV++))
+    if [ $DEV -gt 15 ]; then
+      echo touchpad device not found - exiting
+      exit 1
+    fi
+  fi
+done
+echo -ne 'r\nq\n' | $PROMPT mxt-app -d i2c-dev:$DEV-004a &>/dev/null
+echo -ne 'r\nq\n' | $PROMPT mxt-app -d i2c-dev:$(expr $DEV+1)-004b &>/dev/null
+echo touchpad device i2c-dev:$DEV-004a reset
+
