0cf94d59a7df39a1c64cb1c6e206ca5e1272b4e0
Raphael Simon <simon.raphael@gmail.com>
2016-08-16 01:15:12 -0700

    Use upstream patches for sound support!
    Upgrade to Linux 4.7

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,27 +4,18 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LINUX=`readlink -f $DIR/../build/linux-patched`
 
 if [[ $1 == "--help" ]]; then
-  echo "Build linux 4.5 Samus"
-  echo "This script generates a patched Linux 4.5 kernel tree containing the code necessary to"
-  echo "enable sound, screen and keyboard brightness on the chromebook Pixel 2."
+  echo "Build linux 4.7 Samus"
+  echo "This script generates a patched Linux 4.7 kernel tree containing the code necessary to"
+  echo "enable sound on the chromebook Pixel 2."
+  echo "The patches are from https://lkml.org/lkml/2016/8/14/207"
   echo "The script also generates Debian and ArchLinux packages."
-  echo "Usage: build.sh [--nopatch|branch tag]"
-  echo "--nopatch skips creation of patched tree allowing for incremental builds"
-  echo "branch is git branch, e.g. linux-4.5.y"
-  echo "tag is git tag, e.g. v4.5.1"
   exit 0
 fi
-export branch="$1"
-if [[ "$branch" == "" ]]; then
-  export branch="linux-4.5.y"
-fi
-export tag="$2"
+export tag="$1"
 if [[ "$tag" == "" ]]; then
-  export tag="v4.5.1"
+  export tag="v4.7"
 fi
-if [[ "$1" != "--nopatch" ]]; then
-  $DIR/patch.sh $branch $tag
-fi
+$DIR/patch.sh $tag
 if [ $? -ne 0 ]; then
   exit 1
 fi
--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,82 +1,45 @@
 #!/bin/bash
+#!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LINUX=`readlink -f $DIR/../build/linux-patched`
-CHROMEOS=`readlink -f $DIR/../build/chromiumos-chromeos-3.14`
-ORIGIN=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-BRANCH=linux-4.5.y
-TAG=v4.5.1
+TAG=v4.7
 
 if [ $# -gt 0 ]; then
   if [ "$1" == "--help" ]; then
-    echo "usage: $0 [BRANCH] [TAG]"
+    echo "usage: $0 [TAG]"
     exit 0
   fi
+  TAG=$1
 fi
 
-if [ $# -gt 1 ]; then
-  if [ "$2" == "--help" ]; then
-    echo "usage: $0 [BRANCH] [TAG]"
-    exit 0
-  fi
-  BRANCH=$1
-fi
-
-if [ $# -gt 2 ]; then
-  if [ "$3" == "--help" ]; then
-    echo "usage: $0 [BRANCH] [TAG]"
-    exit 0
-  fi
-  TAG=$2
-fi
-
-echo This script will clone two complete copies of the kernel source code.
-echo The first copy from the official \'Linus\' tree and the second from the chromium tree.
-echo This takes a while and uses a lot of disk space - you\'ve been warned.
-read -p 'Are you sure? (y/n)' -n 1 -r
+echo This script applies the patches from https://lkml.org/lkml/2016/8/14/207 to a newly checked out
+echo kernel tree using the given git tag.
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if [ ! -d $LINUX ]; then
-    git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $LINUX
-  fi
-  if [ ! -d $CHROMEOS ]; then
-    git clone https://chromium.googlesource.com/chromiumos/third_party/kernel $CHROMEOS
-  fi
-fi
 if [ ! -d $LINUX ]; then
   echo $LINUX does not exist, cannot proceed.
   exit 1
 fi
-if [ ! -d $CHROMEOS ]; then
-  echo $CHROMEOS does not exist, cannot proceed.
-  exit 1
-fi
 
-echo Resetting repos
+echo Resetting repo
 cd $LINUX
+git fetch origin
 git checkout .
 git clean -qfdx
-git checkout $BRANCH
+git checkout master
 git checkout $TAG
-cd $CHROMEOS
-git clean -qfdx
-git checkout chromeos-3.14
+git pull origin master
 if [ $? -ne 0 ]; then
   exit 1
 fi
-cd $DIR
+git checkout $TAG
 
-echo Creating patches
-./create_patch.sh generated.patch
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-./apply_patch.sh
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-cp bdw-rt5677.c $LINUX/sound/soc/intel/boards
+echo Patching
+cp $DIR/*.patch .
+patch -p1 < codec.patch
+patch -p1 < driver.patch
+
 echo
 echo Successfully patched Linux!!
 echo You may compile and install it with e.g.:
