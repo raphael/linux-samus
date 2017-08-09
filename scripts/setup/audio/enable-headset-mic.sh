7be0da7bf74376b348bff7deca93bfe510c288f0
Jon Kirkman <jonkirkman@gmail.com>
2017-08-09 11:40:57 -0400

    updated mic scripts to dynamically target the correct device

--- scripts/setup/audio/enable-headset-mic.sh
+++ scripts/setup/audio/enable-headset-mic.sh
@@ -2,12 +2,14 @@
 
 # see: https://github.com/GalliumOS/galliumos-distro/issues/100#issuecomment-241534837
 
-amixer -c 0 cset name='Sto1 ADC MIXL ADC2 Switch' off
-amixer -c 0 cset name='Sto1 ADC MIXR ADC2 Switch' off
-amixer -c 0 cset name='Local DMICs Switch' off
-amixer -c 0 cset name='Remote DMICs Switch' off
-amixer -c 0 cset name='IF1 ADC1 Swap Mux' L/L
-amixer -c 0 cset name='Sto1 ADC MIXL ADC1 Switch' on
-amixer -c 0 cset name='Sto1 ADC MIXL ADC1 Switch' on
-amixer -c 0 cset name='Headset Mic Switch' on
+CARD="$(aplay -l | grep -Eo '^card ([0-9]): bdwrt5677' | sed 's/card //' | sed 's/:.*$//')"
+
+amixer -c $CARD cset name='Sto1 ADC MIXL ADC2 Switch' off
+amixer -c $CARD cset name='Sto1 ADC MIXR ADC2 Switch' off
+amixer -c $CARD cset name='Local DMICs Switch' off
+amixer -c $CARD cset name='Remote DMICs Switch' off
+amixer -c $CARD cset name='IF1 ADC1 Swap Mux' L/L
+amixer -c $CARD cset name='Sto1 ADC MIXL ADC1 Switch' on
+amixer -c $CARD cset name='Sto1 ADC MIXL ADC1 Switch' on
+amixer -c $CARD cset name='Headset Mic Switch' on
 
